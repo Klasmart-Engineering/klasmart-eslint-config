e95e1dd8b3827e796c58e814c23189d9d5cedeef
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-06-29 12:50:23 +0900

    rename file

--- react.js
+++ react.js
@@ -9,7 +9,7 @@ module.exports = {
         },
         ecmaVersion: 12,
         sourceType: `module`,
-        project: `react.tsconfig.json`,
+        project: `tsconfig.react.json`,
     },
     plugins: [ `react` ],
     rules: {
